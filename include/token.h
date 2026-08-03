8902a3e9d3320930c3c17207978edea72de867ce
RadiantEgg <2173590735@qq.com>
2026-08-03 19:55:53 +0800

    修改CMakeList.txt结构，将源文件编译为静态库，设置test测试，完成token.c

--- include/token.h
+++ include/token.h
@@ -25,10 +25,10 @@ typedef struct
                     // 没有按照类型直接存double或者string，因为只识别序列，不计算含义
 } Token;
 
-Token *token_create(TokenType type, char *lexeme);      // 创建一个 Token，并复制传入的 lexeme，让 Token 获得所有权
+Token *token_create(TokenType type, const char *lexeme);      // 创建一个 Token，并复制传入的 lexeme，让 Token 获得所有权
 
 void token_free(Token *token);
 
-void token_print(Token *token);
+void token_print(const Token *token);
 
 #endif
---
+++ src/token.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "token.h"
+
+Token *token_create(TokenType type, const char *lexeme)
+{
+    Token *token = malloc(sizeof(Token));
+    
+    // 错误处理...
+
+    token->type = type;
+    token->lexeme = malloc(strlen(lexeme) + 1);
+
+    // 错误处理...rollback
+
+    strcpy(token->lexeme, lexeme);
+
+    return token;
+}
+
+void token_free(Token *token)
+{
+    free(token->lexeme);
+
+    free(token);
+}
+
+// 改进
+// X Macro: 维护一次自动生成enum和string映射
+
+static const char *token_type_to_string(TokenType type)
+{
+    switch(type)
+    {
+        case TOKEN_EOF:
+            return "EOF";
+        case TOKEN_IDENTIFIER:
+            return "IDENTIFIER";
+        case TOKEN_NUMBER:
+            return "NUMBER";
+        case TOKEN_PLUS:
+            return "PLUS";
+        case TOKEN_MINUS:
+            return "MINUS";
+        case TOKEN_STAR:
+            return "STAR";
+        case TOKEN_SLASH:
+            return "SLASH";
+        case TOKEN_LPAREN:
+            return "LPAREN";
+        case TOKEN_RPAREN:
+            return "RPAREN";
+
+        // 错误处理...
+
+        default:
+            return "Unknown TokenType";
+    }
+}
+
+
+void token_print(const Token *token)    // print 只访问，readonly
+{
+    printf("TokenType: %s\n", token_type_to_string(token->type));
+    printf("TokenLexeme: %s\n", token->lexeme);
+}
